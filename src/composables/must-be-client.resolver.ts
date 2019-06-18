77e1e80accfaae96fec970a883eaf48181c615bb
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-06-18 19:36:34 -0300

    :sparkles: add mor composables

---
+++ src/composables/must-be-client.resolver.ts
@@ -0,0 +1,18 @@
+import {ComposableResolver} from "./composable.resolver";
+import {ResolverContext} from "../interfaces/ResolverContextInterface";
+import {GraphQLFieldResolver} from "graphql";
+import {CollaboratorAccessTypeEnum} from "../models/CollaboratorAccessModel";
+
+export const mustBeClient: ComposableResolver<any, ResolverContext> =
+  (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
+    return (parent, args, context: ResolverContext, info) => {
+      const collaboratorTypes = [
+        "CLIENT",
+      ];
+
+      if (collaboratorTypes.indexOf(<"CLIENT">context.entityAuthenticated!.loginType) === -1)
+        throw new Error("You don't have permission to do that action");
+
+      resolver(parent, args, context, info);
+    }
+  };
---
+++ src/composables/must-be-collaborator-admin.resolver.ts
@@ -0,0 +1,18 @@
+import {ComposableResolver} from "./composable.resolver";
+import {ResolverContext} from "../interfaces/ResolverContextInterface";
+import {GraphQLFieldResolver} from "graphql";
+import {CollaboratorAccessTypeEnum} from "../models/CollaboratorAccessModel";
+
+export const mustBeCollaboratorAdmin: ComposableResolver<any, ResolverContext> =
+  (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
+    return (parent, args, context: ResolverContext, info) => {
+      const collaboratorTypes = [
+        CollaboratorAccessTypeEnum.ADMIN,
+      ];
+
+      if (collaboratorTypes.indexOf(<CollaboratorAccessTypeEnum>context.entityAuthenticated!.loginType) === -1)
+        throw new Error("You don't have permission to do that action");
+
+      resolver(parent, args, context, info);
+    }
+  };
